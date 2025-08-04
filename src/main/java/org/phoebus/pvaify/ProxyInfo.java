8c3bdd8a2744416e3309de1fc20db4b939d1a5fb
kasemir <ky9@ornl.gov>
2025-08-04 15:36:57 -0400

    RPC ...:listClients

--- src/main/java/org/phoebus/pvaify/ProxyInfo.java
+++ src/main/java/org/phoebus/pvaify/ProxyInfo.java
@@ -10,16 +10,20 @@ package org.phoebus.pvaify;
 import static org.phoebus.pvaify.Proxy.logger;
 
 import java.time.Instant;
+import java.util.Collection;
 import java.util.Set;
 import java.util.logging.Level;
 
 import org.epics.pva.data.PVADouble;
 import org.epics.pva.data.PVAInt;
 import org.epics.pva.data.PVAString;
+import org.epics.pva.data.PVAStringArray;
 import org.epics.pva.data.PVAStructure;
 import org.epics.pva.data.nt.PVAScalar;
+import org.epics.pva.data.nt.PVATable;
 import org.epics.pva.data.nt.PVATimeStamp;
 import org.epics.pva.server.PVAServer;
+import org.epics.pva.server.PVAServer.ClientInfo;
 import org.epics.pva.server.ServerPV;
 
 /** PVA PVs with  proxy status
@@ -27,8 +31,11 @@ import org.epics.pva.server.ServerPV;
  */
 class ProxyInfo
 {
+    private final PVAServer server;
     private final PVATimeStamp stamp = new PVATimeStamp();
-    private final ServerPV pvtotal_pv, connected_pv, unconnected_pv, search_pv, client_rate_pv, server_rate_pv;
+    private final ServerPV pvtotal_pv, connected_pv, unconnected_pv,
+                           search_pv, client_rate_pv, server_rate_pv,
+                           list_clients_pv;
     private final PVAStructure pvtotal_data, connected_data, unconnected_data, search_data, client_rate_data, server_rate_data;
     private final Set<String> info_pv_names;
 
@@ -38,6 +45,7 @@ class ProxyInfo
      */
     public ProxyInfo(final String prefix, final PVAServer server) throws Exception
     {
+        this.server = server;
         pvtotal_data = new PVAStructure(prefix + "pvtotal",
                          PVAScalar.SCALAR_STRUCT_NAME_STRING,
                          new PVAInt("value", 0),
@@ -92,12 +100,15 @@ class ProxyInfo
                 stamp);
         server_rate_pv = server.createPV(server_rate_data.getName(), server_rate_data);
 
+        list_clients_pv = server.createPV(prefix + "listClients", this::listClients);
+
         info_pv_names = Set.of(pvtotal_pv.getName(),
                                connected_pv.getName(),
                                unconnected_pv.getName(),
                                search_pv.getName(),
                                client_rate_pv.getName(),
-                               server_rate_pv.getName());
+                               server_rate_pv.getName(),
+                               list_clients_pv.getName());
 
         logger.log(Level.CONFIG, "Info PVs: " + info_pv_names);
     }
@@ -171,5 +182,30 @@ class ProxyInfo
         {
             logger.log(Level.WARNING, "Cannot update info PVs", ex);
         }
+    }
+
+    /** List info about clients to this proxy
+     *  @param parameters Optional parameters (ignored)
+     *  @return List of clients
+     *  @throws Exception on error
+     */
+    private PVAStructure listClients(final PVAStructure parameters) throws Exception
+    {
+        final Collection<ClientInfo> clients = server.getClientInfos();
+        final int N = clients.size();
+        final String[] addr = new String[N], auth = new String[N];
+        int i = 0;
+        for (ClientInfo client : clients)
+        {
+            addr[i] = client.address().getAddress().getHostAddress() + ":" + client.address().getPort();
+            auth[i] = client.authentication().toString();
+            ++i;
+        }
+
+        return new PVAStructure("clients", PVATable.STRUCT_NAME,
+                new PVAStringArray(PVATable.LABELS_NAME, "Address", "Authentication"),
+                new PVAStructure(PVATable.VALUE_NAME, "",
+                                 new PVAStringArray("addr", addr),
+                                 new PVAStringArray("auth", auth)));
     }
 }
